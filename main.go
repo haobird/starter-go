4e11175e1f3c06300ce59a5072bd74871587ba42
jiebaolong <jiebaolong@ctfo.com>
2021-04-26 15:37:17 +0800

    增加保活 函数

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"starter/module"
+	"syscall"
 )
 
 func main() {
@@ -10,4 +13,17 @@ func main() {
 	tran := module.NewTranslation("english")
 	new := tran.Convert(str)
 	fmt.Println(new)
+	keepAlive()
+}
+
+func keepAlive() {
+	//合建chan
+	c := make(chan os.Signal)
+	//监听指定信号 ctrl+c kill
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGUSR2)
+	//阻塞直到有信号传入
+	fmt.Println("启动")
+	//阻塞直至有信号传入
+	s := <-c
+	fmt.Println("退出信号", s)
 }
